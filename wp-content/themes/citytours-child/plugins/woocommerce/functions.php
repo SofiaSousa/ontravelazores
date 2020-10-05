ee1d80274ec4a7f84298e0ad9f782b1424f1aa93
Sofia Sousa <csofiamsousa@gmail.com>
2020-10-05 23:05:01 +0100

    feat: Remove Woocommerce (Emails) menu

--- wp-content/themes/citytours-child/plugins/woocommerce/functions.php
+++ wp-content/themes/citytours-child/plugins/woocommerce/functions.php
@@ -14,8 +14,8 @@ function ot_wc_init() {
 	add_filter( 'woocommerce_endpoint_order-received_title', 'ot_wc_order_received_title', 10, 2 );
 	add_filter( 'woocommerce_order_button_text', 'ot_wc_order_button_text' );
 	add_filter( 'woocommerce_get_item_data', 'ot_wc_extra_booking_info', 10, 2 );
-
 	add_filter( 'woocommerce_form_field_args', 'ot_wc_form_field_args', 10, 3 );
+	add_action( 'woocommerce_checkout_create_order_line_item', 'ot_wc_checkout_create_order_line_item', 20, 4 );
 
 	// Remove actions.
 	remove_action( 'woocommerce_order_status_changed', 'ct_woocommerce_payment_complete', 50, 4 );
@@ -87,7 +87,7 @@ function ot_wc_extra_booking_info( $item_data, $cart_item ) {
 					$price      = esc_attr( $service['price'] );
 
 					$item_data[] = array(
-						'name'  => __( $title . '&nbsp;(+'.$price.'&nbsp;€)', 'citytours' ),
+						'name'  => __( $title . '&nbsp;(+' . $price . '&nbsp;€)', 'citytours' ),
 						'value' => $quantity,
 					);
 				}
@@ -126,4 +126,56 @@ function ot_wc_form_field_args( $args, $key, $value ) {
 	$args['input_class'][] = 'form-control';
 
 	return $args;
+}
+
+
+/**
+ * Add extra data to tour booking item on booking details after checkout.
+ *
+ * @param Object $item           The item.
+ * @param String $cart_item_key  The cart item key.
+ * @param Array  $values         The values.
+ * @param Array  $order          The order.
+ */
+function ot_wc_checkout_create_order_line_item( $item, $cart_item_key, $values, $order ) {
+	$product_id = $item->get_variation_id() ? $item->get_variation_id() : $item->get_product_id();
+
+	$is_custom_product = false;
+	$post_type         = false;
+
+	$hotel_tour_id = get_post_meta( $product_id, '_ct_post_id', true );
+	if ( ! empty( $hotel_tour_id ) ) {
+			$is_custom_product = true;
+			$post_type         = get_post_type( $hotel_tour_id );
+	}
+
+	if ( $is_custom_product && $post_type ) {
+		if ( 'tour' === $post_type ) {
+			$booking_date = get_post_meta( $product_id, '_ct_booking_date', true );
+			$item->update_meta_data( __( 'Date', 'citytours' ), $booking_date );
+
+			$booking_time = get_post_meta( $product_id, '_ct_booking_time', true );
+			$item->update_meta_data( __( 'Time', 'citytours' ), $booking_time );
+
+			$booking_details = get_post_meta( $product_id, '_ct_booking_info', true );
+			$item->update_meta_data( __( 'Adults (+11 Years)', 'citytours' ), $booking_details['adults'] );
+			$item->update_meta_data( __( 'Children (3 - 10 Years)', 'citytours' ), $booking_details['kids'] );
+			$item->update_meta_data( __( 'Infants (0 - 2 Years)', 'citytours' ), $booking_details['infants'] );
+
+			$add_services = get_post_meta( $product_id, '_ct_add_service' );
+			$add_services = $add_services[0];
+
+			if ( ! empty( $add_services ) ) {
+				foreach ( $add_services as $service ) {
+					$service_id = esc_attr( $service['service_id'] );
+					$title      = esc_attr( $service['title'] );
+					$quantity   = esc_attr( $service['qty'] );
+					$price      = esc_attr( $service['price'] );
+
+					$item->update_meta_data( __( $title . '&nbsp;(+'.$price.'&nbsp;€)', 'citytours' ), $quantity );
+
+				}
+			}
+		}
+	}
 }
