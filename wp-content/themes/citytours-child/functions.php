2cb3ac1c97a07b0025270a7d978b94352b8ddfeb
Sofia Sousa <csofiamsousa@gmail.com>
2021-11-28 20:25:04 +0000

    feat: Remove fb chat script

--- wp-content/themes/citytours-child/functions.php
+++ wp-content/themes/citytours-child/functions.php
@@ -257,37 +257,6 @@ if ( ! function_exists( 'ct_tour_generate_conf_mail' ) ) {
 	}
 }
 
-// Script for FB chat.
-add_action(
-	'wp_footer',
-	function() {
-		echo '<!-- Load Facebook SDK for JavaScript -->
-		<div id="fb-root"></div>
-		<script>
-		window.fbAsyncInit = function() {
-			FB.init({
-				xfbml            : true,
-				version          : "v9.0"
-			});
-		};
-
-		(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, "script", "facebook-jssdk"));</script>
-
-		<!-- Your Chat Plugin code -->
-		<div class="fb-customerchat"
-			attribution=install_email
-			page_id="1757371964491287"
-			theme_color="#1cbbb4">
-		</div>';
-	}
-);
-
 // EGO API script.
 // _egoiaq.push(["setClientId", "419232"]);
 // _egoiaq.push(["setTrackerUrl", u+"collect"]);
